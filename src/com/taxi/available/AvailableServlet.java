575e66cffc9a5b23c797b00ccac243687f88c391
susanta <susanta@s>
2017-06-28 09:44:58 +0530

    Some modification in index.html

--- src/com/taxi/available/AvailableServlet.java
+++ src/com/taxi/available/AvailableServlet.java
@@ -12,12 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 public class AvailableServlet extends HttpServlet  {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		int x=3;
+		int available=3;
+		//general setting
 		PrintWriter pw=res.getWriter();
 		res.setContentType("text/html");
 		pw.println("<body bgColor='green'>");
-		pw.println("<center style=color:black>Hi, There "+x+" Taxis are available for You");
-		pw.println("<a href='trap.jsp'>BOOK HERE</a>");
+		
+	
 		String s=req.getParameter("txtResult");
 		
 		pw.println("your access time is "+s);
@@ -31,10 +32,32 @@ public class AvailableServlet extends HttpServlet  {
 		        result += Character.getNumericValue(character);
 		    }
 		}
+		pw.println("<center><u><h1>BOOK YOUR TAXI</h1></u></center>");
 		pw.println("<br>");
-		pw.println("Hi You Reserve your taxi for "+result+" hour");
+		pw.println("<input type='radio' value='taxi1' name='t'>TAXI1</input>");
+		pw.println("<input type='radio' value='taxi2' name='t'>TAXI2</input>");
+		pw.println("<input type='radio' value='taxi3' name='t'>TAXI3</input>");
 		
-		pw.println("<body>");
+	
+		pw.println("<br>");
+		int wanted=1;
+		try {
+			Thread.sleep(2000);
+
+			pw.println("Hi You Reserve your taxi for "+result+" hour");
+		
+			pw.println("<center style=color:black>Hi, There 1 Taxis are available for You");
+			int available1=available-wanted;
+			pw.println("Now total available of taxi is"+available1);
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		pw.println("</body>");
 		
 		
 	}
